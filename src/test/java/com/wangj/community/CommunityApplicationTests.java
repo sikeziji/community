8b27299b958653f5a8ad15ff38d1b3cae561245f
sikeziji <1931696152@qq.com>
2020-03-09 08:44:08 +0800

    add index and bootstrap

---
+++ src/test/java/com/wangj/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangj.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
