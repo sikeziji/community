32f180f77734696e3835e0d31c758cc9be725ff7
sikeziji <1931696152@qq.com>
2020-03-11 18:05:29 +0800

    add github User callback没有成功

---
+++ src/main/java/com/wangj/community/controller/AuthorizeController.java
@@ -0,0 +1,36 @@
+package com.wangj.community.controller;
+
+import com.wangj.community.dto.AccessTokenDTO;
+import com.wangj.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    /**
+     *
+     * @param code
+     * @param state
+     * @return
+     */
+    @GetMapping("/callback")
+    public String callBack(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("6e7f06ea113a34a9fe9d");
+        accessTokenDTO.setClient_secret("878fda83bd7c0c2bb535aa2a9ca55c87de3ee985");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+
+        return null;
+    }
+
+}
