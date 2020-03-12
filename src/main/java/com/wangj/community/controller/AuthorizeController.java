146b06d1644dc8d881a3fa1b03eec4d392a14a2a
sikeziji <1931696152@qq.com>
2020-03-12 18:20:47 +0800

    测试完成连接H2数据库成功，并可以添加登录数据到数据库中

--- src/main/java/com/wangj/community/controller/AuthorizeController.java
+++ src/main/java/com/wangj/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.wangj.community.controller;
 
 import com.wangj.community.dto.AccessTokenDTO;
 import com.wangj.community.dto.GithubUser;
+import com.wangj.community.mapper.UserMapper;
+import com.wangj.community.module.User;
 import com.wangj.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,6 +27,10 @@ public class AuthorizeController {
     private String clientSecret;
     @Value("${github.redirect.uri}")
     private String redirectUri;
+
+
+    @Autowired
+    private UserMapper userMapper;
 
     /**
      * @param code
@@ -41,12 +48,20 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        if (user != null) {
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
             //登录成功，写cookie 和session
-            request.getSession().setAttribute("user", user);
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("githubUser", githubUser);
             return "redirect:/";
         } else {
+            System.out.println("登录失败");
             //登录失败，重新登录
             return "redirect:/";
         }
--- src/main/java/com/wangj/community/mapper/UserMapper.java
+++ src/main/java/com/wangj/community/mapper/UserMapper.java
@@ -1,12 +1,12 @@
 package com.wangj.community.mapper;
 
-        import com.wangj.community.module.User;
-        import org.apache.ibatis.annotations.Insert;
-        import org.apache.ibatis.annotations.Mapper;
+import com.wangj.community.module.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 
-        @Mapper
-        public interface UserMapper {
+@Mapper
+public interface UserMapper {
 
-        @Insert("insert into user {name,account_id,token,gmt_create,gmt_modified} values {#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified}}")
-        void insert(User user);
-        }
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
